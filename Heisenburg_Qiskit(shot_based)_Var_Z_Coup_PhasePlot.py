b287822ff3f7b6e57647c8620daa79e5fdd190d8
AMicajah <andrewmcox1223@gmail.com>
2025-12-18 02:29:54 -0500

    update structure, apply Qiskit Estimator

--- Heisenburg_Qiskit(shot_based)_Var_Z_Coup_PhasePlot.py
+++ Heisenburg_Qiskit(shot_based)_Var_Z_Coup_PhasePlot.py
@@ -2,118 +2,116 @@ import numpy as np
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 from qiskit.quantum_info import Pauli
+from qiskit_aer.primitives import Estimator as AerEstimator
+from qiskit.quantum_info import SparsePauliOp
+import time
 import matplotlib.pyplot as plt
 
 # Coupling Amplitudes
 J_XY = 1.0
 JZ_values = np.linspace(-2, 2, 20)
+L = 8 # system size
+tlist = np.linspace(0, 20, 25)
+trotter_slices = 4
+shots = 200
 
-L = 3 # system size
-tlist = np.linspace(0, 20, 50)
-trotter_slices = 2
+dt_full = tlist[1] - tlist[0]
+dt_small = dt_full / trotter_slices
 
-def run_qiskit_trotter_shotbased(J_XY, J_Z, L, tlist, trotter_slices=5, shots=500):
+estimator = AerEstimator()
 
-    dt_full = tlist[1] - tlist[0]
-    dt_small = dt_full / trotter_slices
+def build_hamiltonian_op(L, J_XY, J_Z):
+    paulis = []
+    coeffs = []
 
-    sim = AerSimulator()
+    for i in range(L-1):
+        # XX
+        label = ['I']*L
+        label[L-1-i], label[L-2-i] = 'X','X'
+        paulis.append(''.join(label))
+        coeffs.append(J_XY)
 
-    def build_hamiltonian_terms(L, J_XY, J_Z):
-        terms = []
-        for i in range(L-1):
-            # XX and YY terms
-            for p in ['XX','YY']:
-                label = ['I']*L
-                label[L-1-i], label[L-2-i] = p[0], p[1]
-                terms.append((J_XY, Pauli(''.join(label))))
-            # ZZ term
-            label = ['I']*L
-            label[L-1-i], label[L-2-i] = 'Z','Z'
-            terms.append((J_Z, Pauli(''.join(label))))
-        return terms
+        # YY
+        label = ['I']*L
+        label[L-1-i], label[L-2-i] = 'Y','Y'
+        paulis.append(''.join(label))
+        coeffs.append(J_XY)
+
+        # ZZ
+        label = ['I']*L
+        label[L-1-i], label[L-2-i] = 'Z','Z'
+        paulis.append(''.join(label))
+        coeffs.append(J_Z)
+
+    return SparsePauliOp(paulis, coeffs)
+
     
-    # Build one Trotter slice
-    def build_trotter_step():
-        qc = QuantumCircuit(L)
+def build_trotter_step(J_XY, J_Z):
+    qc = QuantumCircuit(L)
+
+    for i in range(L-1):
+        
+        # XX
         theta = J_XY * dt_small
-        for i in range(L-1):
+        qc.h(i); qc.h(i+1)
+        qc.cx(i, i+1)
+        qc.rz(2*theta, i+1)
+        qc.cx(i, i+1)
+        qc.h(i); qc.h(i+1)
 
-            # XX
-            qc.h(i); qc.h(i+1)
-            qc.cx(i,i+1)
-            qc.rz(2*theta, i+1)
-            qc.cx(i,i+1)
-            qc.h(i); qc.h(i+1)
+        # YY
+        qc.rx(np.pi/2, i); qc.rx(np.pi/2, i+1)
+        qc.cx(i, i+1)
+        qc.rz(2*theta, i+1)
+        qc.cx(i, i+1)
+        qc.rx(-np.pi/2, i); qc.rx(-np.pi/2, i+1)
 
-            # YY
-            qc.rx(np.pi/2, i); qc.rx(np.pi/2, i+1)
-            qc.cx(i,i+1)
-            qc.rz(2*theta, i+1)
-            qc.cx(i,i+1)
-            qc.rx(-np.pi/2, i); qc.rx(-np.pi/2, i+1)
+        # ZZ
+        theta_Z = J_Z * dt_small
+        qc.cx(i, i+1)
+        qc.rz(2*theta_Z, i+1)
+        qc.cx(i, i+1)
 
-            # ZZ
-            theta_Z = J_Z * dt_small
-            qc.cx(i,i+1)
-            qc.rz(2*theta_Z, i+1)
-            qc.cx(i,i+1)
+    return qc
 
-        return qc
+def run_qiskit_trotter_estimator(J_XY, J_Z, L, tlist, trotter_slices):
+    hamiltonian_op = build_hamiltonian_op(L, J_XY, J_Z)
+    trotter_step = build_trotter_step(J_XY, J_Z)
 
-    # Hamiltonian terms for this set of couplings
-    terms = build_hamiltonian_terms(L, J_XY, J_Z)
-
-    # Trotter slice
-    trotter_step = build_trotter_step()
-
-    # Expectation estimator
-    def estimate_pauli(pauli, evolved):
-        qc = evolved.copy()
-
-        for q, axis in enumerate(str(pauli)[::-1]):
-            if axis == 'X':
-                qc.h(q)
-            elif axis == 'Y':
-                qc.sdg(q); qc.h(q)
-
-        qc.measure_all()
-        result = sim.run(qc, shots=shots).result()
-        counts = result.get_counts()
-
-        exp = 0
-        for bitstring, count in counts.items():
-            parity = 1
-            for q, axis in enumerate(str(pauli)[::-1]):
-                if axis != 'I' and bitstring[::-1][q] == '1':
-                    parity *= -1
-            exp += parity * (count/shots)
-
-        return exp
-
-    # time evolution
     energies = []
-    evolved = QuantumCircuit(L)   # Start in |000…0>
-    
-    for step_idx, t in enumerate(tlist):
+    evolved = QuantumCircuit(L)
 
-        # Add one delta t of evolution
+    for t_idx, t in enumerate(tlist):
+        t0 = time.time()
+        
+        # Trotter evolution
         for _ in range(trotter_slices):
-            evolved = evolved.compose(trotter_step)
+            evolved.compose(trotter_step, inplace=True)
+        t_trotter = time.time() - t0
 
-        # Measure <H>
-        E = 0
-        for coeff, p in terms:
-            E += coeff * estimate_pauli(p, evolved)
+        t1 = time.time()
+        
+        # Estimator call
+        job = estimator.run(circuits=evolved, observables=hamiltonian_op, shots=shots)
+        energy = job.result().values[0]
+        t_estimator = time.time() - t1
 
-        energies.append(E)
+        energies.append(energy)
+
+        #print(f"Time step {t_idx}: Trotter {t_trotter:.3f}s, Estimator {t_estimator:.3f}s")
 
     return energies
 
+
 energy_vs_JZ = []
-for J_Z in JZ_values:
-    energies = run_qiskit_trotter_shotbased(J_XY, J_Z, L, tlist, trotter_slices=2, shots=500)
+
+start_total = time.time()
+for J_Z_idx, J_Z in enumerate(JZ_values):
+    start_jz = time.time()
+    energies = run_qiskit_trotter_estimator(J_XY, J_Z, L, tlist, trotter_slices)
     energy_vs_JZ.append(energies[-1])
+    print(f"J_Z={J_Z:.2f} done in {time.time() - start_jz:.2f}s")
+print(f"Total runtime for all J_Z: {time.time() - start_total:.2f}s")
     
 plt.plot(JZ_values, energy_vs_JZ, 'o-')
 plt.xlabel("J_Z / J_XY")
